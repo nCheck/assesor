47d333e3c3ad5e7c0f5a6d6978b90a2816293d81
Suyash <suyash.sreekumar@gmail.com>
2018-06-20 10:15:12 +0530

    mongoos url added

--- data/db.js
+++ data/db.js
@@ -1,3 +1,9 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+	dbUrl	 = 'mongodb://sanes4:sanes4ever@ds163330.mlab.com:63330/assesor';
 
+
+mongoose.connect(dbUrl);
+mongoose.connection.on('connected' , () =>{
+	console.log('connected')
+});
 
