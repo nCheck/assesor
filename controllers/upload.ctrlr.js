a1b88e86ef8c01f804d9001de091e302c94691d7
Unknown <shreyabhujbal27@gmail.com>
2018-06-24 00:07:12 +0530

    upload of files without controllers

--- controllers/upload.ctrlr.js
+++ controllers/upload.ctrlr.js
@@ -1,14 +1,15 @@
 // Your Requirments goes here
+var express=require("express");
+var app    = express();
+
+router.get('/',function(res,req)=>{
+	res.render('app',{title:'Upload File'});
+});
+//   post nhi aya :(
 
 
 
-
-
-
-
-
-
-module.exports.uploadFile = (req , res) =>{
-  // Your upload logic goes here
-  res.send('File Uploaded')
-}
+// module.exports.uploadFile = (req , res) =>{
+ // Your upload logic goes here
+//   res.send('File Uploaded')
+// }
