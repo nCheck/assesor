6aad177fdb73644fc0a4a03f552a1c472a404207
Nehal Kalnad <yenwiikae@gmail.com>
2018-06-23 08:49:33 +0530

    made some changes

--- app.js
+++ app.js
@@ -8,20 +8,6 @@ var toCtrl = require('./controllers/tool.ctrlr');
 var uploadCtrl = require('./controllers/upload.ctrlr');
 app.use(parser.urlencoded({extended:true}))
 
-// app.get('/',function(req,resp){
-// 	resp.render('co_form.ejs');
-// })
-// app.post('/',coCtrl.addOne);
-//
-// app.get('/display',coCtrl.getAll );
-// app.get('/add/tools', coCtrl.sendBoth);
-// app.post('/add/tools' , coCtrl.insertTool);
-//
-// app.get('/tools' , toCtrl.getAll );
-// app.post('/tools' , toCtrl.addOne);
-// app.get('/toolform' , function (req, res) {
-// 	res.render('toolsAdd.ejs');
-// })
 
 app.get('/upload' , (req ,res)=>{
 	res.render('upload.ejs');
