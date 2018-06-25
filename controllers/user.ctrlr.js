44377e1ee52238c7270f118c991f85b4f279c39a
Suyash <suyash.sreekumar@gmail.com>
2018-06-25 23:44:07 +0530

    basic register and login feature

--- controllers/user.ctrlr.js
+++ controllers/user.ctrlr.js
@@ -15,7 +15,20 @@ module.exports.getAll = function (req , res) {
 	});
 	
 };
-
+//===To display all users =============
+module.exports.displayAll=(req,res)=>{
+	var username =req.body.username,
+		password =req.body.password;
+	User.find({},function(err,user){
+		
+		if(err)
+			console.log("Error spotted",err);
+		else if(username === user[0].username && password === user[0].password)
+			res.render('admin.ejs',{user:user})
+			res.render('display.ejs',{user:user})
+			
+	});
+}
 module.exports.addOne = (req, res)=> {
 	User.create({
 		username : req.body.username,
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var exp=require('express'),
 	router=exp.Router(),
 	userctrlr=require('../controllers/user.ctrlr.js');
-router
-	.route('/login')
-	.get((req,res)=>res.render('login.ejs'))
-	.post(userctrlr.getAll)
+
+
+module.exports=router
