bceb07e03e8cbb449f221abb36021d6aca70f931
Nehal Kalnad <yenwiikae@gmail.com>
2018-06-27 11:45:43 +0530

    xlsx working, good work shreya

--- data/xlsx.js
+++ data/xlsx.js
@@ -4,23 +4,13 @@ const ejs = require('ejs');
 const path = require('path');
 const app = express();
 
-    const XLSX = require('xlsx');
-    const workbook = XLSX.readFile('./data/name.xlsx');
-    //const sheet_name_list = workbook.SheetNames;
-    //console.log(XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]))
+const XLSX = require('xlsx');
+const workbook = XLSX.readFile('./data/name.xlsx');
 
-  var header ="Avg Test"; //Header will come here eg. "Total"
-  //var header="Attendance (46)";
-  //var header ="Test1 (20)";
-  //var header ="Test2 (20);
-  var flag=0;
-  const sheet = workbook.Sheets[workbook.SheetNames[0]];
+var header ="Test1 (20)";
+var flag=0;
+const sheet = workbook.Sheets[workbook.SheetNames[0]];
  var range = XLSX.utils.decode_range(sheet['!ref']);
-//var colTarget = 6;//Find out this
-//var rowTarget = 5;// And this Find out this , by traversing. two forloops ayenge ,  also u may encounter undefined during beginning
-// // //need to take care of that.
- //console.log(range.s.r);
- //console.log(range.s.c);
 
 for (var rowNum = range.s.r ; rowNum <= range.e.r; rowNum++)
 {
@@ -51,10 +41,8 @@ console.log("col"+colTarget);
 
 var student = 0 , aboveAvg = 0 , totalMarks = 0;
 for (let rowNum = rowTarget; rowNum <= range.e.r; rowNum++) {
-    // Example: Get second cell in each row, i.e. Column "B"
     const Cell = sheet[XLSX.utils.encode_cell({r: rowNum, c: colTarget})];
-    //console.log(Cell);
-    if(Cell==undefined){
+    if(Cell==undefined  || isNaN(Cell.w)){
       break;
     }
     else{
