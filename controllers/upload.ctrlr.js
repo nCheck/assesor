ee11943e1e329b82b9974b799341f8e8f126b47b
Shreya <shreyabhujbal27@gmail.com>
2018-06-28 00:06:20 +0530

    deletion

--- controllers/upload.ctrlr.js
+++ controllers/upload.ctrlr.js
@@ -6,6 +6,7 @@ var parser=require('body-parser');
 
 var app    = express();
 app.use(parser.urlencoded({extended:true}));
+const fs=require('fs');
 
 module.exports.uploadFile = (req , res , next) =>{
 	const storage = multer.diskStorage({
@@ -23,11 +24,33 @@ module.exports.uploadFile = (req , res , next) =>{
 				res.send('Error: No File Selected!');
 			} else {
 				console.log("shitted");
-				next();
-				// xlsx.xlsxCal;
+				 // get the temporary location of the file
+	//var tmp_path = req.file.temp.path;
+	var tmp_path ='./uploads/temp.xlsx';
+	// set where the file should actually exists - in this case it is in the "images" directory
+	//var target_path = './public/images/' + req.file.temp.name;
+	var target_path ='./public/images/'+temp.xlsx;
+	// move the file from the temporary location to the intended location
+	fs.rename('tmp_path', 'target_path', function(err) {
+			if (err) throw err;
+			// delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+			fs.unlink('tmp_path', function(err) {
+					if (err) throw err;
+					res.send('File uploaded to: ' + target_path );
+			});
+	});
 
+	next();
 			}
 		}
 	});
 
 }
+// try now! wait
+
+//in comments i have written meaning of each
+//Hi Shreya, you are just changing location here. haan wait
+//hey nehhhhalllllll
+//this is what i have done for deletion
+// google pe tha
+//kaha ho tum?
--- uploads/timetable-.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo rm /var/lib/dpkg/lock
-sudo dpkg --configure -a
-sudo rm /var/lib/apt/lists/lock
-sudo rm /var/cache/apt/archives/lock
