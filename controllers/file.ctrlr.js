9815213874692029bd77c14ef2b22fa75e57c225
Shreya <shreyabhujbal27@gmail.com>
2018-07-22 00:47:56 +0530

    upload file

--- controllers/file.ctrlr.js
+++ controllers/file.ctrlr.js
@@ -1,6 +1,57 @@
 var mongoose = require('mongoose');
 
+var express=require("express");
+const multer = require('multer');
+const path = require('path');
+var parser=require('body-parser');
 
+var app    = express();
+app.use(parser.urlencoded({extended:true}));
+const fs=require('fs');
+
+module.exports.uploadFile = (req , res , next) =>{
+	const storage = multer.diskStorage({
+	  destination: './uploads/DBMS/2018/time-table',
+	  fname: function(req, file, cb){
+	    cb(null,"temp"  + path.extname(file.originalname));
+	  }
+
+	});
+	var upload = multer({ storage: storage }).single('filename1'); //selected-file
+	upload(req, res, (err) => {
+		if(err){
+			res.render('file-index',{
+				msg:err
+			});
+
+			//res.send('err ' + err);
+		} else {
+			if(req.file == undefined){
+			//res.send('Error: No File Selected!');
+			res.render('file-index',{
+				msg:'Error:No file Selected!!'
+			});
+			} else {
+				console.log("shitted");
+				// next();
+					res.send('done');
+        // res.render('file-index',{
+				//	   msg:'File uploaded',
+				 //
+          //  file:`uploads/DBMS/2018/temp.jpg`
+					//	 file: `DBMS/2018/${req.file.fname}`
+		//	filename:req.query.file,
+        //  file:'uploads/DBMS/2018/${req.query.file}'mp
+
+
+        	//res.send('done');
+      //  });
+
+			}
+		}
+	});
+
+}
 
 
 
